2a328176cc6f8778fba35fad2a1ec8d926ee71ce
Caleb Kavindu Keli <kelicaleb7@gmail.com>
2024-11-08 21:48:00 +0300

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes basename="/school-project">
           <Route index path="school-project/" element={<Home/>}/>
-          <Route  path="school-project/Home" element={<Home/>}/>
+          <Route  path="school-project/home" element={<Home/>}/>
           <Route path="school-project/Product" element={<Product/>}/>
           <Route path="school-project/Cart" element={<Cart/>}/>
         </Routes>
